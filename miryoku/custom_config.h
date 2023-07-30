41708e75cb244cd6ca7ec78bcb4253e8c1a2d2a9
Ben Hunter <code@benhunter.me>
2023-07-30 14:58:58 -0500

    Add custom config for Corne Wireless nice!nano from BeeKeeb

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -1,3 +1,13 @@
 // Copyright 2021 Manna Harbour
 // https://github.com/manna-harbour/miryoku
 
+#define MIRYOKU_ALPHAS_QWERTY
+#define MIRYOKU_TAP_QWERTY
+#define MIRYOKU_CLIPBOARD_WIN
+#define MIRYOKU_NAV_VI
+
+// 2023-07-30
+// Added for nice!view on corne wireless from BeeKeeb.
+&nice_view_spi {
+ cs-gpios = <&pro_micro 0 GPIO_ACTIVE_HIGH>;
+};
