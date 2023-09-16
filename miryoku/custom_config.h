8a3bf8656398818bb81cca6389385693b5150da8
Ben Hunter <code@benhunter.me>
2023-09-16 14:20:36 -0500

    Define home row mods to match my Moonlander and Planck

--- miryoku/custom_config.h
+++ miryoku/custom_config.h
@@ -14,8 +14,10 @@
 
 // 2023-09-16
 // Override the QWERTY Base Layer
+// Mod-Tap: U_MT(MOD, TAP) 
+// Layer-Tap: U_LT(LAYER, TAP)
 #define MIRYOKU_ALTERNATIVES_BASE_QWERTY \
-&kp Q,             &kp W,             &kp E,             &kp R,             &kp T,             &kp Y,             &kp U,             &kp I,             &kp O,             &kp P,             \
-U_MT(LGUI, A),     U_MT(LALT, S),     U_MT(LCTRL, D),    U_MT(LSHFT, F),    &kp G,             &kp H,             U_MT(LSHFT, J),    U_MT(LCTRL, K),    U_MT(LALT, L),     U_MT(LGUI, SQT),   \
-U_LT(U_BUTTON, Z), U_MT(RALT, X),     &kp C,             &kp V,             &kp B,             &kp N,             &kp M,             &kp COMMA,         U_MT(RALT, DOT),   U_LT(U_BUTTON, SLASH),\
-U_NP,              U_NP,              U_LT(U_MEDIA, ESC),U_LT(U_NAV, SPACE),U_LT(U_MOUSE, TAB),U_LT(U_SYM, RET),  U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP,              U_NP
+&kp Q, &kp W, &kp E, &kp R, &kp T, &kp Y, &kp U, &kp I, &kp O, &kp P, \
+&kp A, U_MT(LGUI, S), U_MT(LALT, D), U_MT(LCTRL, F), U_MT(LSHFT, G), U_MT(LSHFT, H), U_MT(LCTRL, J), U_MT(LALT, K), U_MT(LGUI, L), &kp SQT, \
+U_LT(U_BUTTON, Z), U_MT(RALT, X), &kp C, &kp V, &kp B, &kp N, &kp M, &kp COMMA, U_MT(RALT, DOT), U_LT(U_BUTTON, SLASH),\
+U_NP, U_NP, U_LT(U_MEDIA, ESC), U_LT(U_NAV, SPACE), U_LT(U_MOUSE, TAB), U_LT(U_SYM, RET), U_LT(U_NUM, BSPC), U_LT(U_FUN, DEL),  U_NP, U_NP
